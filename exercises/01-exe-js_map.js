48eef0fc0a0e1871c7dc472452029af95eb64ceb
TA WDFT BCN Ironhack <ta-web-bcn@ironhack.com>
2022-01-20 15:52:42 +0100

    Added solution

--- exercises/01-exe-js_map.js
+++ exercises/01-exe-js_map.js
@@ -1,24 +1,45 @@
 // ONLY USE THE .map() METHOD FOR THIS EXERCISE
 
-
 // Exercise 1: Capitalize each element of the array - the whole word:
 const fruits = ['pineapple', 'orange', 'mango'];
 
-let uppercaseFruits = fruits.map(() => {
+let uppercaseFruits = fruits.map( (oneFruit) => {
   //your code here...
+
+  return oneFruit.toUpperCase();
+
 })
+
 console.log(uppercaseFruits)
 
-// Answer should be  ['PINEALLPLE', 'ORANGE', 'MANGO'];
+// Answer should be  ['PINEAPPLE', 'ORANGE', 'MANGO'];
 
 
 // Exercise 2: Capitalize the first letter of every city. 
+
 const cities = ['miami', 'barcelona', 'madrid', 'amsterdam', 'berlin', 'sao paulo', 'lisbon', 'mexico city']
 
-let uppercaseCities // your code here...
+// your code here...
 
+let uppercaseCities =  cities.map( city => {
+
+   if (city.includes(' ')) {
+     
+     let splittedArr = city.split(' ');
+     
+     const newArr = splittedArr.map(elem => elem.charAt(0).toUpperCase() + elem.slice(1));
+     
+     return newArr.join(' ');
+   }
+
+   return city[0].toUpperCase() + city.slice(1);
+ 
+});
+
+ console.log(uppercaseCities)
 // Answer should be 
 // [ 'Miami', 'Barcelona', 'Madrid', 'Amsterdam', 'Berlin', 'Sao paulo', 'Lisbon', 'Mexico city', 'Paris' ]
+
 
 // Exercise  3: Create an array with only the names of the restaurants 
 // Note: Be careful here. Info is an object with a key called 'cities' which is an array
@@ -33,12 +54,15 @@ const info = {
    ]  
 }
 
-
 const getRestaurantsNames = (arr) => {
-   //your code here...
+
+   return arr.map( ( restaurant) => restaurant.name)
+
 }
 
-let restaurantsNames = getRestaurantsNames( /* pass your array here*/)
+let restaurantsNames = getRestaurantsNames( info.cities )
+
 console.log(restaurantsNames)
 
 // Answer should be [ 'Mirazur', 'Noma', 'Gaggan', 'Geranium', 'Arpège']
+
